fd0e6d624dda4c59243c3c36af657b5794e349e3
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-11-21 08:27:27 +0100

    fix

--- handlers/aviability.go
+++ handlers/aviability.go
@@ -25,13 +25,13 @@ func (s myAviabilityServer) GetAccommodationCheck(xtx context.Context, in *proto
 	}
 	return out, nil
 }
-func (s myAviabilityServer) GetAllforAccomendation(ctx context.Context, in *protos.GetAllRequest) (*protos.DummyLista, error) {
+func (s myAviabilityServer) GetAllforAccomendation(ctx context.Context, in *protos.GetAllRequest) (*protos.DummyLista3, error) {
 	out, err := s.repo.GetAllforAccomendation(ctx, in)
 	if err != nil {
 		s.logger.Println(err)
 		return nil, err
 	}
-	ss := new(protos.DummyLista)
+	ss := new(protos.DummyLista3)
 	ss.Dummy = out
 	return ss, nil
 }
